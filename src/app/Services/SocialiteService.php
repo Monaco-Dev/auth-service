add2fa20389c00722b5f44147c0c130b70596834
Danniel Libor <32063464+blacksmithHub@users.noreply.github.com>
2024-02-11 11:08:59 +0800

    bug fix

--- src/app/Services/SocialiteService.php
+++ src/app/Services/SocialiteService.php
@@ -104,7 +104,7 @@ class SocialiteService extends Service implements SocialiteServiceInterface
     {
         $driver = Arr::get($request, 'driver');
 
-        return Socialite::driver($driver)->setScopes(['openid', 'email'])->redirect();
+        return Socialite::driver($driver)->stateless()->redirect();
     }
 
     /**
